f97ed95b3970c605dc4ba061cda8ce42ba109ee0
Denis Smetannikov <smet.denis@gmail.com>
2020-07-27 02:41:43 +0300

    init

--- src/Converters/JUnitConverter.php
+++ src/Converters/JUnitConverter.php
@@ -87,9 +87,6 @@ class JUnitConverter extends AbstractConverter
         $junit = new JUnit();
 
         foreach ($sourceCollection->getSuites() as $sourceSuite) {
-
-            $sourceSuite->setFile('1')->
-
             $this->createJUnitNodes($sourceSuite, $junit);
         }
 
